eef7b460a224c8972eddaae39416126e4eb9f59f
JB-Tellez <jb.tellez@gmail.com>
2024-06-25 09:58:47 -0700

    updates portfolio section

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,18 +4,15 @@ import SnackIcon from "./snack-icon";
 export default function Header() {
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center">
-      <Link className="flex items-center justify-center" to="/">
-        <SnackIcon className="h-16 w-16" />
-        <span className="sr-only">Resume</span>
-      </Link>
-      <nav className="ml-auto flex gap-4 sm:gap-6">
-        <Link className="text-sm font-medium hover:underline underline-offset-4" to="#at-a-glance">
-          At a Glance
+        <Link className="flex items-center justify-center" to="#">
+          <SnackIcon className="h-16 w-16" />
+          <span className="sr-only">Full Stack Snacks</span>
         </Link>
-        <Link className="text-sm font-medium hover:underline underline-offset-4" to="#experience">
-          Experience
-        </Link>
-      </nav>
-    </header>
+        <nav className="ml-auto flex gap-4 sm:gap-6">
+          <Link className="text-sm font-medium hover:underline underline-offset-4" to="/resume">
+            Resume
+          </Link>
+        </nav>
+      </header>
   );
 }
