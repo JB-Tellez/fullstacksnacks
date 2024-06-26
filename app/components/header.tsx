69fe785de64ba7de4b4182f9e463afeeea0710e2
JB-Tellez <jb.tellez@gmail.com>
2024-06-26 16:26:54 -0700

    removes sr-only stacks site title

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,9 +4,12 @@ import SnackIcon from "./snack-icon";
 export default function Header() {
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center">
-        <Link className="flex items-center justify-center" to="#">
-          <SnackIcon className="h-16 w-16" />
-          <span className="sr-only">Full Stack Snacks</span>
+        <Link className="flex items-center justify-center"  to="#">
+          <SnackIcon className="h-16 w-16 pt-2" />
+          <div>
+            <p>Full Stack</p>
+            <p>Snacks</p>
+          </div>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link className="text-sm font-medium hover:underline underline-offset-4" to="/resume">
